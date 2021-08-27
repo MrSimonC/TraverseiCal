f1a0c2a98145d9614ce6e59d57bbb29aea91dd5b
Simon Crouch <iamontheinternet@yahoo.com>
2021-08-27 21:26:15 +0100

    add comparer

--- TraverseCalendar/Models/Event.cs
+++ TraverseCalendar/Models/Event.cs
@@ -2,10 +2,24 @@
 
 namespace TraverseCalendar.Models
 {
-    public class Event
+    public class Event : IEquatable<Event>
     {
         public string Uid { get; set; } = string.Empty;
         public string Subject { get; set; } = string.Empty;
         public DateTime DateUTC { get; set; } = DateTime.MinValue;
+
+        public bool Equals(Event? other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            return DateUTC == other.DateUTC
+                && Subject == other.Subject
+                && Uid == other.Uid;
+        }
+        public override bool Equals(object? obj) => Equals(obj as Event);
+        public override int GetHashCode() => (Uid, Subject, DateUTC).GetHashCode();
     }
 }
