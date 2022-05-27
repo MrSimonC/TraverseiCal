5fe9eff5d5fbf6aff04cfbc43e9f3e4793781d4f
Mr Simon C <iamontheinternet@yahoo.com>
2022-05-27 21:34:29 +0000

    add excluded events activity function

---
+++ TraverseCalendar/Constants.cs
@@ -0,0 +1,7 @@
+namespace TraverseCalendar;
+
+public static class Constants
+{
+    public const string tableName = "ExcludedEvents";
+    public const string partitionKey = "events";
+}
--- TraverseCalendar/Functions/EventExclusion.cs
+++ TraverseCalendar/Functions/EventExclusion.cs
@@ -1,22 +1,41 @@
+using Azure;
+using Azure.Data.Tables;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using TraverseCalendar.Models;
+using static TraverseCalendar.Constants;
 
-namespace TraverseiCal.Functions;
+namespace TraverseCalendar.Functions;
 
 public class EventExclusion
 {
     [FunctionName("AddEventExclusion")]
-    [return: Table("ExcludedEvents", "events")]
+    [return: Table(tableName)]
     public static ExcludeEventTableEntity TableOutput(
-        [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] string eventName, 
+        [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] string eventName,
         ILogger log)
     {
         log.LogInformation($"Adding excluded event: {eventName}");
-        return new ExcludeEventTableEntity { 
-            RowKey = Guid.NewGuid().ToString(), 
+        return new ExcludeEventTableEntity
+        {
+            PartitionKey = partitionKey,
+            RowKey = Guid.NewGuid().ToString(),
             Subject = eventName
         };
+    }
+
+    [FunctionName("GetEventExclusions")]
+    public static async Task<ActionResult> TableInputAsync(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+        [Table(tableName, partitionKey)] TableClient tableClient,
+        ILogger log)
+    {
+        log.LogInformation($"Getting events from table: {tableName} with partionKey {partitionKey}");
+        AsyncPageable<ExcludeEventTableEntity> queryResults = tableClient.QueryAsync<ExcludeEventTableEntity>(filter: $"PartitionKey eq '{partitionKey}'");
+        var results = await queryResults.Select(e => e.Subject).ToListAsync();
+        return new OkObjectResult(results);
     }
 }
